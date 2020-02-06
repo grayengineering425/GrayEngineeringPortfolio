68e3ed7854277dea790cf93d7a207b936d86983c
Alex Gray <grayengineering425@gmail.com>
2020-02-06 12:56:45 +0100

    update

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+  constructor()
+  {
+    window.onresize = function ()
+    {
+      var topContactPageWrapper = document.getElementsByClassName("top-contact-page-wrapper")[0] as HTMLElement;
+      var topContactPage        = document.getElementsByClassName("top-contact-page"        )[0] as HTMLElement;
 
-  ngOnInit() {
+      var halfWidth = (topContactPageWrapper.getBoundingClientRect().right - topContactPageWrapper.getBoundingClientRect().left) / 2.0;
+
+      topContactPage.style.borderLeft   = halfWidth.toString() + "px solid transparent";
+      topContactPage.style.borderRight  = halfWidth.toString() + "px solid transparent";
+    }
+  }
+
+  ngOnInit()
+  {
+    var topContactPageWrapper = document.getElementsByClassName("top-contact-page-wrapper")[0] as HTMLElement;
+    var topContactPage        = document.getElementsByClassName("top-contact-page"        )[0] as HTMLElement;
+
+    var halfWidth = (topContactPageWrapper.getBoundingClientRect().right - topContactPageWrapper.getBoundingClientRect().left) / 2.0;
+
+    topContactPage.style.borderLeft   = halfWidth.toString() + "px solid transparent";
+    topContactPage.style.borderRight  = halfWidth.toString() + "px solid transparent";
   }
 
 }
